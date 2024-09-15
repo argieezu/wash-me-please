a5a057c7632485d23e2cf28a1d3d696681f89503
Argiee <112626873+Argiezuu@users.noreply.github.com>
2024-09-15 23:56:06 +0800

    designing admin dashboard

--- LaundrySystem/AdminPages/AdminDashboardForm.Designer.cs
+++ LaundrySystem/AdminPages/AdminDashboardForm.Designer.cs
@@ -28,17 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             panel1 = new Panel();
+            panel12 = new Panel();
+            groupBox2 = new GroupBox();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            bindingSource1 = new BindingSource(components);
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            panel1.SuspendLayout();
+            panel12.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.White;
+            panel1.Controls.Add(panel12);
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(838, 437);
             panel1.TabIndex = 0;
+            // 
+            // panel12
+            // 
+            panel12.BackColor = Color.White;
+            panel12.Controls.Add(dataGridView2);
+            panel12.Controls.Add(dataGridView1);
+            panel12.Controls.Add(groupBox2);
+            panel12.Controls.Add(groupBox1);
+            panel12.Controls.Add(label1);
+            panel12.Dock = DockStyle.Fill;
+            panel12.Location = new Point(0, 0);
+            panel12.Name = "panel12";
+            panel12.Size = new Size(838, 437);
+            panel12.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(434, 254);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(335, 171);
+            groupBox2.TabIndex = 11;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Timeline Revenue";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(38, 254);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(326, 171);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Account Receivable";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(24, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(175, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Monthly Revenue";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(38, 80);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(326, 150);
+            dataGridView1.TabIndex = 12;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(434, 80);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(326, 150);
+            dataGridView2.TabIndex = 13;
             // 
             // AdminDashboardForm
             // 
@@ -48,11 +123,30 @@
             Controls.Add(panel1);
             Name = "AdminDashboardForm";
             Text = "AdminDashboardForm";
+            panel1.ResumeLayout(false);
+            panel12.ResumeLayout(false);
+            panel12.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
+        private Panel panel12;
+        private GroupBox groupBox2;
+        private GroupBox groupBox1;
+        private Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Guna.Charts.WinForms.GunaChart gunaChart1;
+        private Guna.Charts.WinForms.GunaLineDataset gunaLineDataset1;
+        private Guna.Charts.WinForms.GunaPieDataset gunaPieDataset1;
+        private Guna.Charts.WinForms.GunaHorizontalBarDataset gunaHorizontalBarDataset1;
+        private Guna.Charts.WinForms.GunaChart gunaChart2;
+        private DataGridView dataGridView2;
+        private DataGridView dataGridView1;
+        private BindingSource bindingSource1;
     }
 }
